5c19786913f6f2f8fa9e33b205fab22af0574ee8
Jonathan Gomez <jonnygomez156@gmail.com>
2023-10-21 18:14:03 -0700

    added the adblocker

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -105,7 +105,22 @@ function soundcloud () {
     }).then(() => {
         location.href=__uv$config.prefix + __uv$config.encodeUrl('https://soundcloud.com/');
     });
-
+    //adblocker by jigar288
+    setInterval(function(){
+        var source = document.getElementsByTagName('html')[0].innerHTML;
+        var found = source.search("Advertisement");
+        if(found != -1){
+          window.location.reload(true);
+        }
+         }, 1000);
+        
+        function alertFunction() {
+        setTimeout(function(){
+            var btn = document.querySelector(".playControls__play");
+            btn.click();
+        }, 3000);
+        } 
+        window.onload = alertFunction;
 }
 
 function twitch () {
